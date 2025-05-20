a8aeaeb331edcfb0c7c58c55e64d6e6c6b3ca960
unknown <fatihaqila02@gmail.com>
2025-05-21 05:40:41 +0700

    Fix Bug Swagger Utang

--- app/Models/Pengeluaran.php
+++ app/Models/Pengeluaran.php
@@ -4,21 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use OpenApi\Annotations as OA;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 /**
  * @OA\Schema(
- *     schema="Pengeluaran",
- *     type="object",
  *     title="Pengeluaran",
- *     required={"tanggal", "jumlah", "user_id"},
- *     @OA\Property(property="id_pengeluaran", type="integer", example=1),
- *     @OA\Property(property="tanggal", type="string", format="date", example="2025-05-01"),
- *     @OA\Property(property="catatan", type="string", example="Belanja bahan"),
- *     @OA\Property(property="jumlah", type="number", example=150000),
- *     @OA\Property(property="user_id", type="integer", example=1)
+ *     description="Model for Pengeluaran",
+ *     @OA\Xml(
+ *         name="Pengeluaran"
+ *     )
  * )
  */
+
 class Pengeluaran extends Model
 {
     use HasFactory;
